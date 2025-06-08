0d770b0d4b10dcd63654052e1051240633b75cfe
majoralpaca984 <eleonora.troiani84@gmail.com>
2025-06-08 10:58:25 +0200

    import

--- frontend/src/components/Calendar.jsx
+++ frontend/src/components/Calendar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./theme.css";
+import "../style/theme.css";
+
 
 function Calendar({ year, month, appointments, isAdmin }) {
   const navigate = useNavigate();
